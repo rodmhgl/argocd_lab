e42f2dc9013dc1a2cd81b11918b6f09d55ad9bd9
Rod Stewart <rodstewart@gmail.com>
2025-05-22 09:50:21 -0500

    Adds az backend

--- providers.tf
+++ providers.tf
@@ -7,11 +7,19 @@ terraform {
       version = ">= 4.0.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate1337"
+    container_name       = "tfstate"
+    key                  = "demo_argocd.tfstate"
+    use_azuread_auth     = true
+  }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+  subscription_id     = var.subscription_id
+  storage_use_azuread = true
 }
 #endregion
 
